adb1c1429d2e415c1052464162689f6e0d8141ee
Umar Hussain <umar.hussain.da@outlook.com>
2021-12-23 14:32:07 +0000

    adding file handler and changes to log file

--- app/utils.py
+++ app/utils.py
@@ -10,15 +10,24 @@ def logger(name : str) -> logging.Logger:
     #create console handler and set level to debug
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
+
+    #create file handler and set level to warning
+    fh = logging.FileHandler('logs/app.log', mode='w')
+    fh.setLevel(logging.INFO)
     
     #create formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     
     #add formatter to ch
     ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+
     
     #add ch to logger
     logger.addHandler(ch)
+    #add fh to logger
+    logger.addHandler(fh)
+    
     
     return logger
 
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def get_new_files(path : str) -> list:
 
 
     files = list(Path(path).glob('*.xlsx*'))
-    logger.debug(f'{len(files)} files found')
+    logger.info(f'{len(files)} files found')
 
     return files
 
@@ -27,16 +27,17 @@ def get_relevant_columns(file : Path,
                         target_cols : Optional[list] = ['new to nc fusion','player_id','ethnicity']) -> None:
 
     df = pd.read_excel(file, sheet_name='Sheet2', engine='openpyxl')
-    logger.debug(f'the following columns are present in the file: {df.columns.tolist()}')
+    logger.info(f'the following columns are present in the file: {df.columns.tolist()}')
 
     df = df.loc[:,df.columns.str.contains('|'.join(target_cols),case=False)]
-    logger.debug(f'the following columns were found: {df.columns} in {file.name}')
+    logger.info(f'the following columns were found: {df.columns} in {file.name}')
 
     file_dt = create_iso_date(file)
     move_file(file_dt, file.parent.joinpath('processed'))
     
-    df.to_csv(file_dt.parent.joinpath('curated',f"{file_dt.stem}.csv"), index=False)
-
+    curated_file = file_dt.parent.joinpath('curated',f"{file_dt.stem}.csv")
+    df.to_csv(curated_file, index=False)
+    logger.info(f'file was moved to curated folder - {curated_file}')
 
 #create iso date for file
 def create_iso_date(file : Path) -> str:
@@ -49,18 +50,18 @@ def create_iso_date(file : Path) -> str:
 #move file to new location
 def move_file(file : Path, processed_path : Path) -> None:
     
-        logger.debug(f'moving {file.name} to {processed_path}')
+        logger.info(f'moving {file.name} to {processed_path}')
         #create location if not exists
         if not Path(processed_path).is_dir():
             Path(processed_path).mkdir(parents=True)
-            logger.debug(f'created {processed_path}')
+            logger.info(f'created {processed_path}')
         #creat curataed location
         if not Path(file.parent.joinpath('curated')).is_dir():
             Path(file.parent.joinpath('curated')).mkdir(parents=True)
-            logger.debug(f'created {file.parent.joinpath("curated")}')
+            logger.info(f'created {file.parent.joinpath("curated")}')
         
         file.rename(file.parent.joinpath(processed_path).joinpath(file.name))
-        logger.debug(f'moved {file.name} to {processed_path}')
+        logger.info(f'moved {file.name} to {processed_path}')
 
 
     
